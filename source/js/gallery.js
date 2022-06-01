59838eb47224a69db72e2f0a8294c4aaa0112bdc
Pezzzo <watchingthewaves.mix@gmail.com>
2022-06-01 13:05:08 +0300

    improvement of the gallery functionality

--- source/js/gallery.js
+++ source/js/gallery.js
@@ -12,19 +12,73 @@ const modal = document.querySelector('.modal');
 const modalImg = document.querySelector('.modal-popup__img');
 const imgContainer = document.querySelector('.photo-gallery');
 const html = document.querySelector('.page');
+const closeModalButton = document.querySelector('.modal-popup__close-button');
+const imgContent = document.querySelectorAll('.photo-content__img');
 
-imgContainer.addEventListener('click', (evt) => {
-  if (evt.target.classList.contains('photo-content__img')) {
-    modal.classList.add('shown');
-    html.classList.add('page--modal-open');
+// состояние скролла
+const disableScrolling = () => {
+  var x = window.scrollX;
+  var y = window.scrollY;
+  window.onscroll=function(){window.scrollTo(x, y);};
+};
+
+const enableScrolling = () => {
+  window.onscroll=function(){};
+};
+
+// рендер
+// const renderModal = () => {
+
+// }
+
+modal.addEventListener('click', (evt) => {
+  const arrow = evt.target.closest('.arrow');
+  if (arrow) {
+    console.log('qwqwqw')
   }
 });
 
+// открытие и закрытие попапа
+const openModalHandler = (evt) => {
+  const img = evt.target.closest('img');
 
-html.addEventListener('click', (evt) => {
+  if (img) {
+    modal.classList.add('shown');
+    html.classList.add('page--modal-open');
+    disableScrolling();
+  }
+  modalImg.src = `img/${images[1]}`;
+  // modalImg.src = imgContent.src;
+
+
+};
+
+const closeModalMouseHandler = (evt) => {
   if (evt.target.classList.contains('photo-content__img')) {
     return;
   }
   modal.classList.remove('shown');
   html.classList.remove('page--modal-open');
-});
+  enableScrolling();
+};
+
+const closeModalKeyHandler = (evt) => {
+  if (evt.key === 'Esc' || evt.key === 'Escape') {
+      evt.preventDefault();
+      modal.classList.remove('shown');
+      html.classList.remove('page--modal-open');
+      enableScrolling();
+  }
+};
+
+
+
+
+
+const popap = () => {
+  imgContainer.addEventListener('click', openModalHandler);
+  closeModalButton.addEventListener('click', closeModalMouseHandler);
+  window.addEventListener('keydown', closeModalKeyHandler)
+}
+
+popap();
